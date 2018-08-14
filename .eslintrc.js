8b7ea6198b65d536bcbd2e68ce3d0920de7310b9
ps-aux <seriouz.accz@gmail.com>
2018-08-14 21:36:27 +0200

    Initial

---
+++ .eslintrc.js
@@ -0,0 +1,3 @@
+module.exports = {
+    extends: 'ps-aux-react'
+}
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const App = () =>
+    <div>
+        <p>Hello there!</p>
+    </div>
+
+ReactDOM.render(<App/>, document.getElementById('app'))
